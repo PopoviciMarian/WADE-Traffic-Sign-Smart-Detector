d3b749d7fca357b2c85b7c0f4cbc5c09b075e91e
BITDEFENDER\mapopovici <mapopovici@bitdefender.com>
2025-01-20 00:21:48 +0200

    Fix directory path in Docker build step and add Terraform configuration files for Google Cloud deployment

---
+++ infra/main.tf
@@ -0,0 +1,5 @@
+
+resource "google_storage_bucket" "mybucket" {
+  name     = "video-bucket-wade-1"
+  location = "US"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "credentials_file" {
+  description = "Path to the Google Cloud credentials file"
+}
+
+variable "project_id" {
+  description = "The Google Cloud project ID"
+}
+
+variable "region" {
+  description = "The Google Cloud region"
+}

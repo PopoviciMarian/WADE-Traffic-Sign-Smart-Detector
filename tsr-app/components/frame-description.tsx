7ef9ae17be74b5cd5c68485cd88ee8444dbeab4a
BITDEFENDER\mapopovici <mapopovici@bitdefender.com>
2025-02-03 23:05:15 +0200

    Refactor image source path in FrameDescription component for improved accessibility

--- tsr-app/components/frame-description.tsx
+++ tsr-app/components/frame-description.tsx
@@ -39,7 +39,7 @@ export function FrameDescription({ isLoading, ontologyData }: FrameDescriptionPr
               <div key={index} className="flex space-x-4">
                 <div className="flex-shrink-0 w-24 h-24 relative">
                   <Image
-                    src={`https://storage.cloud.google.com/video-bucket-wade-1/signs/Images/${data.id}.png?authuser=3`}
+                    src={`/signs/${data.id}.png`}
                     alt={data.name}
                     fill
                     className="object-contain"
